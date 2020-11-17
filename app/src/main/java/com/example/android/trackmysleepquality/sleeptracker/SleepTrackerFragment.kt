a163623f3e32997bd647b2e5a3ffe7c81767f3bd
Unmesh Chowdhury <unmeshchow@gmail.com>
2020-11-17 19:42:03 +0600

    Added GridLayout with RecyclerView

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -84,6 +85,9 @@ class SleepTrackerFragment : Fragment() {
                     sleepTrackerViewModel.doneShowingSnackbar()
                 }
         })
+
+        val manager = GridLayoutManager(activity, 3/*, GridLayoutManager.HORIZONTAL, false*/)
+        binding.sleepList.layoutManager = manager
 
         val adapter = SleepNightAdapter()
         binding.sleepList.adapter = adapter
